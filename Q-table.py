20081a85ae9f55eca29d7c00b0c7d3495556717e
Rong Nan <467166703@qq.com>
2018-05-30 22:05:07 +0800

    the change of explore rate and steps

--- Q-table.py
+++ Q-table.py
@@ -1,7 +1,7 @@
 import gym
 import numpy as np
 import random
-
+from bokeh.plotting import figure, output_file, show
 env = gym.make('FrozenLake-v0')
 
 num_action = env.action_space.n
@@ -12,17 +12,22 @@ q_table = np.zeros([num_state, num_action])
 epsodes = 10000
 max_step = 100
 gamma = 0.95
-min_explord_rate = 0.01
+min_explore_rate = 0.01
 learn_rate = 0.8
 decay_rate = 0.01
-explord_rate = 1
+explore_rate = 1
 
+#show explore rate data
+rate_data = []
+#show max_step in every episode
+show_steps = []
 for epsode in  range(epsodes):
+    rate_data.append(explore_rate)
     state = env.reset()
 
     for step in range(max_step):
         real_random = random.uniform(0,1)
-        if real_random < explord_rate:
+        if real_random < explore_rate:
             action = env.action_space.sample()
         else:
             action = np.argmax(q_table[state,:])
@@ -31,11 +36,28 @@ for epsode in  range(epsodes):
         q_table[state, action] = q_table[state][action] + \
                                  learn_rate * (gamma*np.max(q_table[new_state,:]) - q_table[state, action] + reward)
         if done:
+            show_steps.append(step+1)
             break
         state = new_state
-    explord_rate -= (1 - min_explord_rate) / epsodes
+    if len(show_steps) <= epsode:
+        show_steps.append(max_step)
+    explore_rate -= (1 - min_explore_rate) / epsodes
 
 print(q_table)
+
+
+p = figure(title="the explore rate in every episode", width=800, height=800)
+p.title.align = "center"
+#plot the steps in every episode
+p.line(y=show_steps, x=range(len(show_steps)), color= "navy", legend = "stpes")
+#plot the change of export_rate
+p.line(y=[item*100 for item in rate_data], x=range(len(rate_data)), color="red", legend = "explore rate")
+p.xaxis.axis_label = "episode"
+p.yaxis.axis_label = "explore rate: % / steps"
+
+output_file("explore_rate.html")
+show(p)
+
 
 all_reward = 0
 for episode in range(100):
